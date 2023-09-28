75e0a67f82a5bf121bcd2d5f1596ac909a5235de
adong <781461379@qq.com>
2023-09-28 20:55:55 +0800

    01.v2-解耦 类职责单一

---
+++ src/com/minis/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -0,0 +1,24 @@
+package com.minis.beans.factory.xml;
+
+import com.minis.beans.BeanDefinition;
+import com.minis.beans.factory.BeanFactory;
+import com.minis.core.Resource;
+import org.dom4j.Element;
+
+public class XmlBeanDefinitionReader {
+    BeanFactory beanFactory;
+
+    public XmlBeanDefinitionReader(BeanFactory beanFactory) {
+        this.beanFactory = beanFactory;
+    }
+
+    public void loadBeanDefinitions(Resource resource) {
+        while (resource.hasNext()) {
+            Element element = (Element) resource.next();
+            String beanID = element.attributeValue("id");
+            String beanClassName = element.attributeValue("class");
+            BeanDefinition beanDefinition = new BeanDefinition(beanID, beanClassName);
+            this.beanFactory.registerBeanDefinition(beanDefinition);
+        }
+    }
+}
--- src/com/minis/test/Test1.java
+++ src/com/minis/test/Test1.java
@@ -1,10 +1,11 @@
 package com.minis.test;
 
 
+import com.minis.beans.BeansException;
 import com.minis.context.ClassPathXmlApplicationContext;
 
 public class Test1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BeansException {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
         AService aService = (AService) ctx.getBean("aservice");
         aService.sayHello();
